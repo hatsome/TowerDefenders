3c1979eab10cb17811423cc4c115b2f17ad573d5
David Staub <david_staub@hotmail.com>
2019-12-19 08:18:29 +0100

    remove unused usings

--- Assets/Scripts/BaseProperties.cs
+++ Assets/Scripts/BaseProperties.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "New BaseProperties", menuName = "BaseProperties")]
 public class BaseProperties : ScriptableObject
--- Assets/Scripts/WaveProperties.cs
+++ Assets/Scripts/WaveProperties.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "New WaveProperties", menuName = "WaveProperties")]
 public class WaveProperties : ScriptableObject
